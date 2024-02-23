1d26ac807d505cd64b80ab66117f52ecd917f586
tamago572 <110117095+tamago572@users.noreply.github.com>
2024-02-23 16:36:50 +0900

    明度で01表示するコードを追加

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ import sys
 import ffmpeg
 import numpy as np
 import os
+import time
 
 args = sys.argv
 
@@ -14,15 +15,34 @@ else:
     print("存在する動画ファイルを入力してください")
     sys.exit()  # スクリプトを終了
 
+
+frame_width = 48
+frame_height = 36
+color_depth = 1     # グレースケールは1、RGBは3
+
+
+print("動画を変換します")
 out, _ = (
     ffmpeg
     .input(movie)
-    .output('pipe:', format='rawvideo', pix_fmt='gray', s='48x36', r=30)
+    .output('pipe:', format='rawvideo', pix_fmt='gray', s='48x36', r=15)
     .run(capture_stdout=True)
 )
-
-arr = (
+print("動画を変換しました")
+print("配列に格納しています")
+frames = (
     np
     .frombuffer(out, np.uint8)
-    .reshape([-1, 36, 48])
+    .reshape([-1, frame_height, frame_width, color_depth])
 )
+print("配列に格納しました")
+
+for frame in frames:
+    # フレーム内の各ピクセルの明度を取得し、"0"という文字を黒色で表示します
+    for y in range(frame_height):
+        for x in range(frame_width):
+            brightness = frame[y, x]
+            # ANSIエスケープコードを使用して文字色を設定します
+            # 38;2;R;G;Bmは前景色をRGB値で設定します
+            print(f"\033[38;2;{brightness};{brightness};{brightness}m0", end="")
+        print("\033[0m")  # 色設定をリセットして改行
