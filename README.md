4773d6fb5faebe4bd4b3496eb8ff4b682ad9f86e
potesala <110117095+tamago572@users.noreply.github.com>
2024-02-23 18:19:22 +0900

    一応完成したが、音が遅延する

--- README.md
+++ README.md
@@ -1,1 +1,16 @@
 # 0or1-movie
+
+## 使い方
+
+初回は、requirements.txtをインストールします
+
+```shell
+pip install -r requirements.txt
+```
+
+以下のコマンドで0or1で動画を再生できます。
+```shell
+python main.py [videofile] [audiofile]
+```
+
+ターミナルの設定で、行間を1.0にすると、きれいに表示されます。
--- main.py
+++ main.py
@@ -3,29 +3,40 @@ import ffmpeg
 import numpy as np
 import os
 import time
+import pygame
 
 args = sys.argv
 
 movie = args[1]
+audio = args[2]
 # 引数に渡された動画ファイルが存在するか確認
-is_file = os.path.isfile(movie)
-if is_file:
+movie_Is_file = os.path.isfile(movie)
+if movie_Is_file:
     print(f"{movie} が見つかりました")
 else:
     print("存在する動画ファイルを入力してください")
     sys.exit()  # スクリプトを終了
 
 
+# 引数に渡された音声ファイルが存在するか確認
+audio_Is_file = os.path.isfile(audio)
+if audio_Is_file:
+    print(f"{movie} が見つかりました")
+else:
+    print("存在する音声ファイルを入力してください")
+    sys.exit()  # スクリプトを終了
+
+
 frame_width = 48
 frame_height = 36
-color_depth = 1     # グレースケールは1、RGBは3
+color_depth = 3     # グレースケールは1、RGBは3
 
 
 print("動画を変換します")
 out, _ = (
     ffmpeg
     .input(movie)
-    .output('pipe:', format='rawvideo', pix_fmt='gray', s='48x36', r=15)
+    .output('pipe:', format='rawvideo', pix_fmt='rgb24', s='48x36', r=15)
     .run(capture_stdout=True)
 )
 print("動画を変換しました")
@@ -36,13 +47,29 @@ frames = (
     .reshape([-1, frame_height, frame_width, color_depth])
 )
 print("配列に格納しました")
+print("動画を再生します")
 
-for frame in frames:
-    # フレーム内の各ピクセルの明度を取得し、"0"という文字を黒色で表示します
-    for y in range(frame_height):
-        for x in range(frame_width):
-            brightness = frame[y, x]
-            # ANSIエスケープコードを使用して文字色を設定します
-            # 38;2;R;G;Bmは前景色をRGB値で設定します
-            print(f"\033[38;2;{brightness};{brightness};{brightness}m0", end="")
-        print("\033[0m")  # 色設定をリセットして改行
+
+def playMovie():
+    # subprocess.Popen(["ffplay", "-nodisp", "-autoexit", "-loglevel", "quiet", movie])
+    for frame in frames:
+        for row in frame:
+            for pixel in row:
+                r, g, b = pixel
+                if r == 0 and g == 0 and b == 0:
+                    print("\033[30m" + "a", end="")
+                else:
+                    print("\033[38;2;{};{};{}m".format(r, g, b) + "a", end="")
+            print("\033[0m")  # Reset ANSI color
+        time.sleep(1/15)  # フレームレートに合わせて待機
+        print("\033c")
+
+
+pygame.mixer.init()
+pygame.mixer.music.load(audio)
+pygame.mixer.music.play(1)
+
+playMovie()
+# pygame.mixer.music.stop()
+
+print("動画の再生が完了しました")
